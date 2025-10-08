b73f8ea6600866960678b58d4150875514907b11
Victor Matia <vmatir@outlook.com>
2025-10-08 20:31:00 +0100

    Refactor MainWindow initialization and enhance logging in MainWindowViewModel; manage window state during game execution

--- SageCore.Launcher/ViewModels/MainWindowViewModel.cs
+++ SageCore.Launcher/ViewModels/MainWindowViewModel.cs
@@ -6,18 +6,23 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Diagnostics.CodeAnalysis;
+using Avalonia.Controls;
 using Microsoft.Extensions.Logging;
 
 namespace SageCore.Launcher.ViewModels;
 
 internal sealed partial class MainWindowViewModel : ViewModelBase
 {
-    private readonly ILogger _logger;
+    private readonly ILogger _sageGameLogger;
+    private readonly Window _mainWindow;
 
     public string Greeting { get; } = "Welcome to Avalonia!";
 
-    public MainWindowViewModel()
+    public MainWindowViewModel([NotNull] Window mainWindow)
     {
+        _mainWindow = mainWindow;
+
         // TODO: Allow configuration of logging level and output (e.g., file, console, etc.)
         // For now, we will log to the console with a default log level.
 #if DEBUG
@@ -30,7 +35,7 @@ internal sealed partial class MainWindowViewModel : ViewModelBase
             builder.AddConsole().SetMinimumLevel(logLevel)
         );
 
-        _logger = loggerFactory.CreateLogger<SageGame>();
+        _sageGameLogger = loggerFactory.CreateLogger<SageGame>();
 
         // Just run a default game instance for now.
         // TODO: Add options to control the type of game and where the game files are located.
@@ -39,7 +44,19 @@ internal sealed partial class MainWindowViewModel : ViewModelBase
 
     public void StartSageGame()
     {
-        using SageGame sageGame = new(_logger);
-        sageGame.Run();
+        _mainWindow.WindowState = WindowState.Minimized;
+
+        try
+        {
+            using SageGame sageGame = new(_sageGameLogger);
+            sageGame.Run();
+        }
+        finally
+        {
+            _mainWindow.WindowState = WindowState.Normal;
+            _mainWindow.Activate();
+        }
+
+        // TODO: Catch the exception and show a user-friendly error message in the UI.
     }
 }
