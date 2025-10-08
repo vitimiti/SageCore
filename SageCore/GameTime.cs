166097348df01714f14eedb6dd5e52b268f8f5a3
Victor Matia <vmatir@outlook.com>
2025-10-08 19:58:49 +0100

    Implement GameTime class and integrate timing updates in SageGame

---
+++ SageCore/GameTime.cs
@@ -0,0 +1,93 @@
+// -----------------------------------------------------------------------
+// <copyright file="GameTime.cs" company="SageCore Contributors">
+// 2025 Copyright (c) SageCore Contributors. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Diagnostics;
+
+namespace SageCore;
+
+/// <summary>
+/// Provides timing values for the game loop, including total elapsed time and delta time between frames.
+/// </summary>
+[DebuggerDisplay("{TotalTime} (+{DeltaTime}) @ {TickRatio} ticks/stopwatch tick")]
+internal class GameTime
+{
+    private readonly Stopwatch _stopwatch = new();
+
+    private long _startTime;
+    private long _lastTime;
+    private long _currentTime;
+
+    /// <summary>
+    /// Gets the ratio of TimeSpan ticks to Stopwatch ticks.
+    /// </summary>
+    public static double TickRatio => (double)TimeSpan.TicksPerSecond / Stopwatch.Frequency;
+
+    /// <summary>
+    /// Gets the total elapsed time since the game started.
+    /// </summary>
+    public TimeSpan TotalTime { get; private set; }
+
+    /// <summary>
+    /// Gets the time elapsed since the last update.
+    /// </summary>
+    public TimeSpan DeltaTime { get; private set; }
+
+    /// <summary>
+    /// Gets the total elapsed time in seconds since the game started.
+    /// </summary>
+    public float TotalTimeSeconds { get; private set; }
+
+    /// <summary>
+    /// Gets the time elapsed in seconds since the last update.
+    /// </summary>
+    public float DeltaTimeSeconds { get; private set; }
+
+    /// <summary>
+    /// Gets the maximum allowed delta time to prevent large jumps in time (e.g., when debugging or if the game is paused).
+    /// </summary>
+    private static TimeSpan MaxDeltaTime => TimeSpan.FromMilliseconds(100);
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GameTime"/> class and starts the stopwatch.
+    /// </summary>
+    public GameTime()
+    {
+        _stopwatch.Start();
+        _startTime = _stopwatch.ElapsedTicks;
+        _lastTime = _startTime;
+        _currentTime = _startTime;
+    }
+
+    /// <summary>
+    /// Updates the timing values. Should be called once per frame.
+    /// </summary>
+    public void Update()
+    {
+        _lastTime = _currentTime;
+        _currentTime = _stopwatch.ElapsedTicks;
+
+        // Cache calculated values
+        TotalTime = new TimeSpan((long)((_currentTime - _startTime) * TickRatio));
+
+        TimeSpan delta = new((long)((_currentTime - _lastTime) * TickRatio));
+        DeltaTime = delta > MaxDeltaTime ? MaxDeltaTime : delta;
+
+        TotalTimeSeconds = (float)TotalTime.TotalSeconds;
+        DeltaTimeSeconds = (float)DeltaTime.TotalSeconds;
+    }
+
+    /// <summary>
+    /// Resets the game time to the current time, effectively restarting the timing.
+    /// </summary>
+    public void Reset()
+    {
+        _startTime = _stopwatch.ElapsedTicks;
+        _lastTime = _startTime;
+        _currentTime = _startTime;
+    }
+}
--- SageCore/SageGame.cs
+++ SageCore/SageGame.cs
@@ -6,6 +6,7 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 
 namespace SageCore;
@@ -13,13 +14,31 @@ namespace SageCore;
 /// <summary>
 /// A basic game class to represent a Sage game instance.
 /// </summary>
-/// <param name="logger">The logger instance to use for logging.</param>
+/// <param name="logger">The logger instance for logging game events.</param>
+[DebuggerDisplay("IsRunning = {IsRunning}")]
 public sealed class SageGame(ILogger logger) : IDisposable
 {
+    private readonly GameTime _gameTime = new();
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the game is currently running.
+    /// </summary>
+    private bool IsRunning { get; set; }
+
     /// <summary>
     /// Runs the game instance.
     /// </summary>
-    public void Run() { }
+    public void Run()
+    {
+        IsRunning = true;
+        while (IsRunning)
+        {
+            _gameTime.Update();
+
+            // Game update and rendering logic would go here.
+            IsRunning = false; // For demonstration, we stop after one loop.
+        }
+    }
 
     /// <summary>
     /// Disposes of the game instance.
