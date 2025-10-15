dfa5e1bf7e14b77061236bca864c8e63c4121474
Victor Matia <vmatir@outlook.com>
2025-10-15 16:26:56 +0100

    Add SuppressMessage attribute to ScienceType enum for design compliance

--- SageCore.Common/IniFileSystem/ScienceType.cs
+++ SageCore.Common/IniFileSystem/ScienceType.cs
@@ -6,9 +6,12 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace SageCore.Common.IniFileSystem;
 
 /// <summary>
 /// Specifies the type of science or technology in the game.
 /// </summary>
+[SuppressMessage("Design", "CA1008: Enums should have zero value", Justification = "No 'None' type needed.")]
 public enum ScienceType { }
