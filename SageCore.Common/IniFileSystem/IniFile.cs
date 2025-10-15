d0cc0fbe1b19914cad0f35180381420f986e137c
Victor Matia <vmatir@outlook.com>
2025-10-15 21:56:07 +0100

    Add delegate for parsing fields in INI files and initialize lookup dictionary

--- SageCore.Common/IniFileSystem/IniFile.cs
+++ SageCore.Common/IniFileSystem/IniFile.cs
@@ -14,7 +14,24 @@ namespace SageCore.Common.IniFileSystem;
 public class Ini
 {
     /// <summary>
+    /// A delegate for parsing a field in an INI file.
+    /// </summary>
+    /// <typeparam name="TInstance">The type of the instance being parsed.</typeparam>
+    /// <typeparam name="TStore">The type of the store used during parsing.</typeparam>
+    /// <param name="ini">The INI file being parsed.</param>
+    /// <param name="instance">The instance being parsed.</param>
+    /// <param name="store">The store used during parsing.</param>
+    /// <returns>The parsed instance.</returns>
+    public delegate TInstance FieldParse<TInstance, TStore>(Ini ini, TInstance instance, TStore store)
+        where TStore : class; // ref objects to be able to modify them in place
+
+    /// <summary>
     /// The maximum number of characters allowed per line in an INI file.
     /// </summary>
     public const int MaxCharsPerLine = 0x04_04;
+
+    /// <summary>
+    /// A lookup dictionary for list record types in INI files.
+    /// </summary>
+    public static readonly Dictionary<string, int> LookupListRecord = [];
 }
