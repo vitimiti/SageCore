9f1c745ae2d4eac1a816d87463c496dae7f397bd
Victor Matia <vmatir@outlook.com>
2025-10-15 16:25:33 +0100

    Add ScienceType enum to define types of science or technology in the game

---
+++ SageCore.Common/IniFileSystem/ScienceType.cs
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------
+// <copyright file="ScienceType.cs" company="SageCore Contributors">
+// 2025 Copyright (c) SageCore Contributors. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SageCore.Common.IniFileSystem;
+
+/// <summary>
+/// Specifies the type of science or technology in the game.
+/// </summary>
+public enum ScienceType { }
